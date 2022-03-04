40763e27903c6321576ee942beab1fe9fa301949
Vinicio <88550456+VinicioJesus@users.noreply.github.com>
2022-03-04 17:01:11 -0300

    adicioinando arquivo controllerContatos.php

---
+++ Controller/controllerContatos.php
@@ -0,0 +1,37 @@
+<?php
+
+    /**************************************************
+     * Objetivo: Arquivo responsável pela manipulação de dados de contatos
+     * Obs( Este arquivo fará a ponte entre a View e a Model)
+     * Autor: Vinicio
+     * Data: 04/03/2022
+     * Versão: 1.0 
+     *************************************************/
+    
+     //função para receber dados da View e encaminhar para a model (inserir)
+    function inserirContato()
+    {
+
+    }
+
+    //função para receber dados da View e encaminhar para a model (atualizar)
+    function atualizarContato()
+    {
+
+    }
+
+    //função para realizar a exclusão de um contato
+    function excluirContato()
+    {
+
+    }
+
+    //função para solicitar os dados da model e encaminhar a lista 
+    //de contatos para a View
+    function listarContato()
+    {
+
+    }
+
+
+?>
